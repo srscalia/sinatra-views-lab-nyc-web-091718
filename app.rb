8f981cff127c3550bdbb2e07ab90a6d1090b0c3a
srscalia <shelby.scalia@gmail.com>
2018-10-08 12:42:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 class App < Sinatra::Base
 
 	get '/' do
-		erb :index
+	    "Hello World"
+	  end
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
 	end
 
 
